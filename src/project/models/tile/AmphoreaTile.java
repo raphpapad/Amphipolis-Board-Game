2b19d1f0bc31b9013d5032d2f29b1d805f1124dd
Raphael Papadakis <papadakisraphael@gmail.com>
2026-05-14 13:33:03 +0300

    Update AmphoreaTile.java

--- src/project/models/tile/AmphoreaTile.java
+++ src/project/models/tile/AmphoreaTile.java
@@ -3,7 +3,10 @@
   * This is a subclass of the class FindingTile that finds the amphoras tiles. Basicly it calculates 
   * the poitns of amphora.
   */
-public class AmphoreaTile  extends FindingTile{
+public class AmphoreaTile  extends FindingTile {
+	private boolean mat = false;
+    private String color; 
+	
 	public AmphoreaTile(int id) {
     	super(id);
     	this.mat = false;
@@ -30,10 +33,8 @@ public class AmphoreaTile  extends FindingTile{
 	public void amphora_materialize() {
 		this.mat = true;
 	}
+	
     public void setColor(String color) {
         this.color = color;
     }
-    
-    private boolean mat = false;
-    private String color; 
 }
