c33a09fc2efb61dd9885a8d08f30ff0df35eca46
Raphael Papadakis <papadakisraphael@gmail.com>
2026-05-14 13:21:54 +0300

    Update Game.java

--- src/project/controller/Game.java
+++ src/project/controller/Game.java
@@ -1,6 +1,6 @@
 package project.controller;
  public class Game {
-	 /** Entry point for Game */
+	 /* Entry point for Game */
     public static void main(String[] args) {
     	int stats = 0;
     	Controller GO =  new Controller();
